fceada708f01738d86523f40735952a85a61b0f9
NatawatOhm <natawat.cho@gmail.com>
2023-07-23 23:41:59 +0700

    set token in session storage

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,16 @@
 import React, { useEffect } from 'react';
 
 const Main = () => {
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.hash.substring(1));
+    const token = queryParams.get('access_token') || '';
+    if (token != null) {
+      sessionStorage.setItem('token', token);
+      console.log('1');
+    } else {
+      console.log('2');
+    }
+  }, []);
   return (
     <div>
       <div className="justify-center flex">This is main spotify</div>
